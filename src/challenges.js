b890fc6c01ed8b516fe46de01b9f5986a0809a42
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-21 11:58:56 -0300

    Função que calcula repetição do maior número, de caça ao rato e FizzBuzz

--- src/challenges.js
+++ src/challenges.js
@@ -30,19 +30,51 @@ function footballPoints(wins,ties) {
 };
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(numeros) {
+  let maiorValor = numeros[0];
+  let totalNumeros = 0;
+  for (let index = 0; index < numeros.length; index += 1) {
+    if (numeros[index] > maiorValor) {
+      maiorValor = numeros[index];
+      totalNumeros = 1;
+    } else if (numeros[index] === maiorValor)
+      totalNumeros += 1;
+  }
+  return totalNumeros;
+};
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+function catAndMouse(mouse, cat1, cat2) {
+  let dist1 = Math.abs (mouse - cat1);
+  let dist2 = Math.abs (mouse - cat2);
+  let resultado;
+
+  if (dist1 < dist2) {
+    resultado = 'cat1';
+  } else if (dist2 < dist1) {
+    resultado = 'cat2';
+  } else {
+    resultado = 'os gatos trombam e o rato foge';
+  }
+  return resultado;
+};
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+function fizzBuzz(numero) {
+  let numerosArray = [];
+  for (let index = 0; index < numero.length; index += 1) {
+    if (numero[index] % 3 === 0 && numero[index] % 5 === 0) {
+      numerosArray.push('fizzBuzz');
+    } else if (numero[index] % 3 === 0) {
+      numerosArray.push('fizz');
+    } else if (numero[index] % 5 === 0) {
+      numerosArray.push('buzz');
+    } else {
+      numerosArray.push('bug!');
+    }
+  }
+return (numerosArray);
+};
 
 // Desafio 9
 function encode() {
